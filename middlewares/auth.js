145574990f005f78621da368abbbb4c88807a893
christina-baldwin <christina.baldwin13@yahoo.com>
2025-06-13 10:05:17 +0200

    fixing token names

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret_here";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 export const authenticate = (req, res, next) => {
   const authHeader = req.headers.authorization;
